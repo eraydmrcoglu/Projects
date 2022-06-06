3b42444ba767c90d92dbd3083ec85c91bb27b4a0
Eray Demircioğlu <75967993+eraydmrcoglu@users.noreply.github.com>
2022-06-06 09:39:32 +0300

    Delete typing-speed-app directory

--- typing-speed-app/src/components/Timer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useSelector } from "react-redux";
-
-function Timer() {
-  const countdown = useSelector((state) => state.typing.countdown);
-  return <h1>{countdown < 0 ? 0 : countdown}</h1>;
-}
-
-export default Timer;
--- typing-speed-app/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import wordSlice from "./wordSlice";
-
-export const store = configureStore({
-  reducer: {
-    typing: wordSlice,
-  },
-});
