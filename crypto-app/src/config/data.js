bad0594c96c1a14edac965556702f0d91ebeb9df
Eray Demircioğlu <75967993+eraydmrcoglu@users.noreply.github.com>
2022-06-06 09:00:51 +0300

    Add files via upload

---
+++ crypto-app/src/config/data.js
@@ -0,0 +1,18 @@
+export const chartDays = [
+  {
+    label: "24 Saat",
+    value: 1,
+  },
+  {
+    label: "30 Gün",
+    value: 30,
+  },
+  {
+    label: "3 Ay",
+    value: 90,
+  },
+  {
+    label: "1 Yıl",
+    value: 365,
+  },
+];
