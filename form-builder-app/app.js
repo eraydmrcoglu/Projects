7847f7d51b8c2b1b6c0132f5b380105961251c6a
Eray Demircioğlu <75967993+eraydmrcoglu@users.noreply.github.com>
2022-06-10 09:49:23 +0300

    Add files via upload

---
+++ form-builder-app/app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import DemoBar from './demobar';
+import FormBuilder from "./src/index";
+import * as variables from './variables'
+
+require('./css/application.css.scss');
+
+ReactDOM.render(
+  <FormBuilder.ReactFormBuilder variables={variables} />,
+  document.getElementById('form-builder')
+)
+
+ReactDOM.render(
+  <DemoBar variables={variables} />,
+  document.getElementById('demo-bar')
+)
---
+++ form-builder-app/src/toolbar-item.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Toolbar extends React.Component {
+  render() {
+    return(
+      <li onClick={this.props.onClick}><i className={this.props.data.icon}></i>{this.props.data.name}</li>
+    )
+  }
+}
